114f96675b8e5402dd2e1a6634fc20ead7505b0a
M Hadi Sasmito <keselyoleren@gmail.com>
2023-08-29 20:41:23 +0700

    init

--- src/config/settings.py
+++ src/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-7f#^w@b*kf5qvty(k@*(w)^s5rnt%09ija9$%2j7l_%b@#d@$$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
